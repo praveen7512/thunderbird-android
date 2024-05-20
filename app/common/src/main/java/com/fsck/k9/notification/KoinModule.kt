4764917d3d65b896c968f16666bcad247d4485b9
Praveen Sahu <72389100+praveen7512@users.noreply.github.com>
2024-05-20 11:40:32 -0400

    Update KoinModule.kt
    Added comments to KoinModule.kt

--- app/common/src/main/java/com/fsck/k9/notification/KoinModule.kt
+++ app/common/src/main/java/com/fsck/k9/notification/KoinModule.kt
@@ -2,10 +2,23 @@ package com.fsck.k9.notification
 
 import org.koin.dsl.module
 
+// Define a Koin module to provide dependencies related to notifications
 val notificationModule = module {
+    // Provide a singleton instance of NotificationActionCreator
     single<NotificationActionCreator> {
-        K9NotificationActionCreator(context = get(), defaultFolderProvider = get(), messageStoreManager = get())
+        K9NotificationActionCreator(
+            context = get(),
+            defaultFolderProvider = get(),
+            messageStoreManager = get()
+        )
     }
-    single<NotificationResourceProvider> { K9NotificationResourceProvider(get()) }
-    single<NotificationStrategy> { K9NotificationStrategy(get()) }
+    // Provide a singleton instance of NotificationResourceProvider
+    single<NotificationResourceProvider> {
+        K9NotificationResourceProvider(get())
+    }
+    // Provide a singleton instance of NotificationStrategy
+    single<NotificationStrategy> {
+        K9NotificationStrategy(get())
+    }
 }
+
